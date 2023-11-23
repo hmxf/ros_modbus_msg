5692026cee011f5e76b1b4f0edbb2719bf719d1a
Lvr <Lvrui@lv.com>
2023-11-23 20:24:03 +0800

    Add x-axis synchronous monitoring

--- scripts/listen.py
+++ scripts/listen.py
@@ -1,11 +1,20 @@
 import rospy
 from wrapper_modbus.BaseModbusClient import BaseModbusClient
 
-def speed_ms(ch_x:list):
+def SpeedandMs(ch_x:list):
     tmp_speed = client._readRegisters(ch_x[1],ch_x[2])
     tmp_ms = client._readRegisters(ch_x[3],ch_x[2])
     print(f"{ch_x[0]}: speed: {tmp_speed}  ms:{tmp_ms}")
 
+def Monitor_X_Axis_Position(result):
+    """
+    Monitor whether the position of the two x-axes is consistent, 
+    (And the difference between the two axes is 5 units to meet the requirements
+    """
+    print(result)
+    if abs(result[3] - result[9]) not in (0, 5):
+        rospy.logwarn(f"Warning: The x-axis positions are not synchronized! Value is {abs(result[3] - result[9])}")
+        client.multiAxis_EMERGENCYSTOP()
 
 if __name__ == "__main__":
     rospy.init_node("Listen")
@@ -21,12 +30,10 @@ if __name__ == "__main__":
     ch2 = [' Y ', 10320,2,10350]
     ch3 = [' Z ', 10420,2,10450]
 
-    speed_ms(ch0)
-    speed_ms(ch1)
-    speed_ms(ch2)
-    speed_ms(ch3)
-
+    for chx in [ch0,ch1,ch2,ch3]:
+        SpeedandMs(chx)
 
     while not rospy.is_shutdown():
         rospy.sleep(1)
         result = client.multiAxis_StateRead()
+        Monitor_X_Axis_Position(result)
