f0a1b0cc5a9b91a36d7b2ce1fb5a6b9ca9411948
Lvr <Lvrui@lv.com>
2024-01-16 21:52:39 +0800

    Modify  `_perform_operation` func：addread the current position； rename ：ROS_multiAxis_Origin

--- src/wrapper_modbus/ROS_Modbus.py
+++ src/wrapper_modbus/ROS_Modbus.py
@@ -1,4 +1,4 @@
-from wrapper_modbus.d12_controller_mapping import controller_axis_mapping,date_map,oper_map
+from wrapper_modbus.d12_controller_mapping import controller_axis_mapping,date_map,oper_map,check_complete_map
 from wrapper_modbus.d12_modbus_client import D12ModbusClient
 import rospy
 
@@ -63,8 +63,12 @@ class OperCommand():
         rospy.set_param('complete', 0)
         self.complete = rospy.get_param("complete")
 
+        tmp = 0
+        if oper == 3 or 5:
+            tmp = self.client._readRegisters(check_complete_map[i][0], check_complete_map[i][1])
+
         while self.complete == 0:
-            operation_func(address, values, i)
+            operation_func(address, values, i,tmp)
             self.complete = rospy.get_param("complete")
 
 
@@ -78,6 +82,6 @@ class OperCommand():
             self._perform_operation(i, oper, value, self.client.multiAxis_AbsMoveSpeed)
 
 
-    def ROS_multiAxis_AdvanceOrigin(self, oper, value):
+    def ROS_multiAxis_Origin(self, oper, value):
         for i in range(self.Axis_num):
             self._perform_operation(i, oper, value, self.client.multiAxis_Origin)
